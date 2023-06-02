b462bb48cc10ea25221d610df49280fd2df67218
justbenuk <justbenuk@gmail.com>
2023-06-02 21:19:31 +0100

    initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from '@/components/header/header'
+import Footer from '@/components/footer/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='min-h-screen'>
+      <body className={inter.className + 'min-h-screen flex flex-col justify-between'} style={{backgroundImage: `url("/assets/bg1.jpg")`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+        <Header />
+        <main className='h-[93vh]'>{children}</main>
+        <Footer />
+      </body>
     </html>
   )
 }
